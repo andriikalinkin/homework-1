8b159db8292868ff8b6debbf745b761427ae854c
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-09-15 17:07:05 +0300

    Delete comments

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from urllib.parse import unquote
 
 
-# def parse(query: str) -> dict:
-#     return {}
-#
-#
-# if __name__ == '__main__':
-#     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-#     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-#     assert parse('http://example.com/') == {}
-#     assert parse('http://example.com/?') == {}
-#     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-
-
 def parse_cookie(query: str) -> dict:
     cookie_parts = query.split(';')
     result = {}
