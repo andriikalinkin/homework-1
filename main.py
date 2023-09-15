32dc46a103d090afed29d7b33250339b6e6b077d
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-09-15 17:06:16 +0300

    Delete comments

--- main.py
+++ main.py
@@ -21,14 +21,3 @@ if __name__ == '__main__':
     assert parse('http://example.com/?a=1&a=2&a=3') == {'a': '1'}
     assert parse('http://example.com/?name=John%20Doe') == {'name': 'John Doe'}
     assert parse('http://example.com/?empty=') == {}
-
-
-# def parse_cookie(query: str) -> dict:
-#     return {}
-#
-#
-# if __name__ == '__main__':
-#     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-#     assert parse_cookie('') == {}
-#     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-#     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
